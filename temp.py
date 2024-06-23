44c92e8eb54bc7a3cb3d2a6a7b31ea5606c833c7
varun0401 <varunkulkarni30@gmail.com>
2024-06-23 16:02:44 +0530

    ff

--- temp.py
+++ temp.py
@@ -52,4 +52,4 @@ def transcribe():
     return jsonify({'transcript': transcript, 'summary': summary})
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
